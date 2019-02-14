f5e11260c02acf6a488f4a223d6c8f9e16cf09ad
awiles808 <awiles808@gmail.com>
2019-02-13 20:22:10 -0700

    reactbootstrap

--- search-app/src/App.js
+++ search-app/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-
+          <Jumbotron>
+              <Container>
+                  <h1>Search App</h1>
+                  <p>This is a simple search app</p>
+                </Container>
+          </Jumbotron>
       </div>
     );
   }
